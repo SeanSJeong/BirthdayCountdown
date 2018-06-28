b34107c3d91707348a977eee5162953452937d0d
Sean Jeong <sean.jeong.engineering@gmail.com>
2018-06-28 13:17:50 -0600

    functional button component and styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Picker from "./picker";
+import Button from "./button";
 
 export default class App extends Component {
   render() {
@@ -16,6 +17,8 @@ export default class App extends Component {
         <div className="grid__skew-light-three-box" />
 
         <Picker />
+
+        {Button("Generate Countdown")}
       </div>
     );
   }
