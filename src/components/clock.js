49eca18f2cc31d828e7ebda4dd9c803f17d8f01f
sean1130 <39747545+sean1130@users.noreply.github.com>
2018-06-29 16:04:48 -0600

    fixed timer state bug, and got timer object into clockjs JSX

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,31 +1,30 @@
-import React, { Component } from "react";
+
+import React, { Component } from 'react'
 
 class Clock extends Component {
-  render() {
-    return (
-      <div className="clock">
-        <div className="clock__days clock__box">
-          <label className="clock__title">DAYS</label>
-          <label className="clock__amount">300</label>
-        </div>
-
-        <div className="clock__hours clock__box">
-          <label className="clock__title">HRS</label>
-          <label className="clock__amount">16</label>
-        </div>
-
-        <div className="clock__minutes clock__box">
-          <label className="clock__title">MINS</label>
-          <label className="clock__amount">42</label>
-        </div>
-
-        <div className="clock__seconds clock__box">
-          <label className="clock__title">SECS</label>
-          <label className="clock__amount">22</label>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        const time = this.props.timeRemaining;
+        return (
+            <div className="clock">
+                <div className="clock__days clock__box">
+                    <label className="clock__title">DAYS</label>
+                    <label className="clock__amount">{time.days}</label>
+                </div>
+                <div className="clock__hours clock__box">
+                    <label className="clock__title">HRS</label>
+                    <label className="clock__amount">{time.hours}</label>
+                </div>
+                <div className="clock__minutes clock__box">
+                    <label className="clock__title">MINS</label>
+                    <label className="clock__amount">{time.minutes}</label>
+                </div>
+                <div className="clock__seconds clock__box">
+                    <label className="clock__title">SECS</label>
+                    <label className="clock__amount">{time.seconds}</label>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Clock;
